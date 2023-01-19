a512a3793f3ed6f43524aae7f2f6055d9d2f50b2
Mario Arce <elmariobros96@gmail.com>
2023-01-19 02:00:27 -0800

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Yo merenges",
+    description: "ahora si me dejo no utilzar useEffect",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "mi tercera tarea",
+  },
+];
